23e7e31f376f961ce7b8913212ea0fbf604a5aa1
lloyet <contact@lloyet.dev>
2026-01-15 15:18:22 +0100

    feat(app): add middleware

--- index-payement.js
+++ index-payement.js
@@ -5,9 +5,38 @@ const HOST = "0.0.0.0";
 const PORT = 3001;
 const SECRET = "abcdefghij";
 
+function generateSecureRandomString() {
+	// Human readable alphabet (a-z, 0-9 without l, o, 0, 1 to avoid confusion)
+	const alphabet = "abcdefghijkmnpqrstuvwxyz23456789";
+
+	// Generate 24 bytes = 192 bits of entropy.
+	// We're only going to use 5 bits per byte so the total entropy will be 192 * 5 / 8 = 120 bits
+	const bytes = new Uint8Array(24);
+	crypto.getRandomValues(bytes);
+
+	let id = "";
+	for (let i = 0; i < bytes.length; i++) {
+		// >> 3 "removes" the right-most 3 bits of the byte
+		id += alphabet[bytes[i] >> 3];
+	}
+	return id;
+}
+
+/**
+ * @typedef {Object} Payment
+ * @property {number} id
+ * @property {number} userId
+ * @property {number} price
+ * @property {string} date
+ */
+
+/**
+ * @type {Payment}
+ */
+const payments = [];
+
 const app = express()
-	.get("/payments", async (req, res, next) => {
-		// Bearer TOKEN
+	.use((req, res, next) => {
 		const credentials = req.headers.authorization.split(" ");
 
 		if (credentials.length !== 2 || credentials[0] !== "Bearer") {
@@ -18,6 +47,15 @@ const app = express()
 
 		const payload = jwt.verify(token, SECRET);
 
+		req.toto = payload;
+
+		next();
+	})
+	.get("/payments/:paymentid", async (req, res, next) => {
+		const payload = req.toto;
+
+		console.log(payload);
+		// Bearer TOKEN
 		if (
 			!payload.scope.includes("payments:r") &&
 			!payload.scope.includes("payments:rw")
@@ -29,10 +67,15 @@ const app = express()
 			ok: "vous avez le droit",
 		});
 	})
-	.get("/invoices", async (req, res, next) => {
+	.post("/payments", async (req, res, next) => {
+		// scope obligatoire payments:rw
+
 		res.status(200).json({
 			token,
 		});
+	})
+	.delete("/payments", async (req, res, next) => {
+		// scope obligatoire payments:rw
 	});
 
 app.listen(PORT, HOST, () => {
