06f7921bb8d7e593e1a6eb038fdd227c4a8301c1
lloyet <contact@lloyet.dev>
2026-01-16 12:25:55 +0100

    fix(db): update schema

--- init.sql
+++ init.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS `user` (
 	id			TEXT NOT NULL PRIMARY KEY,
 	email		TEXT NOT NULL,
 	password	TEXT NOT NULL,
-	role_id		TEXT NOT NULL,
+	role_id		TEXT NOT NULL REFERENCES `role`(id),
 	created_at	TEXT NOT NULL
 );
 
@@ -10,6 +10,17 @@ CREATE TABLE IF NOT EXISTS `role` (
 	id			TEXT NOT NULL PRIMARY KEY,
 	name		TEXT NOT NULL,
 	created_at	TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `permission` (
+	id			TEXT NOT NULL PRIMARY KEY,
+	name		TEXT NOT NULL, -- blog -> blog:r -> blog:rw
+);
+
+CREATE TABLE IF NOT EXISTS `role_permission` (
+	id				TEXT NOT NULL PRIMARY KEY,
+	role_id			TEXT NOT NULL REFERENCES `role`(id),
+	permission_id	TEXT NOT NULL REFERENCES `permission`(id) ON DELETE CASCADE
 );
 
 CREATE INDEX `user_email_index` ON `user` (`email`);
